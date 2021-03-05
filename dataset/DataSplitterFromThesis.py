2b2bb76840379572385074081a8fc5e54ed5c784
ipek.karaman <123>
2021-03-05 19:24:05 +0300

    fixed.

--- dataset/DataSplitterFromThesis.py
+++ dataset/DataSplitterFromThesis.py
@@ -1,17 +1,18 @@
 import numpy
 
 import json
+from langdetect import detect
 
 class Thesis(object):
     def __init__(self, data):
         self.__dict__ = json.loads(data)
-FIELD_NAME = "ComputerScience"
+FIELD_NAME = "Chemistry"
 # FIELD_NAME = "Biology"
 
 # EN_SENTENCES_DATASET = '/archive/EnglishSentencesDataset/ComputerScience.txt'
 # TR_SENTENCES_DATASET = '/archive/EnglishSentencesDataset/ComputerScience_tr.txt'
 # THESIS_DATASET = '/archive/thesisOutput/Biology_thesis.txt'
-THESIS_DATASET = '/archive/thesisOutput/Computer_thesis.txt'
+THESIS_DATASET = '/archive/thesisOutput/Chemistry_thesis.txt'
 
 THESIS_file = open(THESIS_DATASET, 'r')
 
@@ -29,7 +30,13 @@ numpy.random.seed(7)
 ####### training ###############33
 en_trainingSentences = []
 tr_trainingSentences=[]
+count=0
+lineCount=0
+totalCount=0
 for line in trainingSet:
+    lineCount=lineCount+1
+    if lineCount%100==0:
+        print("Line: ",lineCount)
     thesis = Thesis(line)
     english_sentences = thesis.englishSentences
     turkishSentences = thesis.turkishSentences
@@ -39,13 +46,35 @@ for line in trainingSet:
         words = sentence.split()
         if len(words) == 1:
               continue
+        try:
+            language=detect(sentence)
+            if language=='tr':
+                continue
+                # tr_trainingSentences.append(sentence)
+                # print(sentence)
+                # count=count+1
+        except:
+            print("&&")
+        # totalCount=totalCount+1
         en_trainingSentences.append(sentence)
     for sentence in turkishSentences:
         words = sentence.split()
         if len(words) == 1:
             continue
+        try:
+            language=detect(sentence)
+            if language=='en':
+                continue
+                # en_trainingSentences.append(sentence)
+                # print(sentence)
+                # count=count+1
+        except:
+            print("&&")
+        totalCount=totalCount+1
         tr_trainingSentences.append(sentence)
 
+print("Count:",count)
+print("Total Count:",totalCount)
 ####### test ###############33
 en_testSentences = []
 tr_testSentences=[]
@@ -59,11 +88,23 @@ for line in testSet:
         words = sentence.split()
         if len(words) == 1:
             continue
+        try:
+            language=detect(sentence)
+            if language=='tr':
+                continue
+        except:
+            print("&&")
         en_testSentences.append(sentence)
     for sentence in turkish_Sentences:
         words = sentence.split()
         if len(words) == 1:
             continue
+        try:
+            language=detect(sentence)
+            if language=='en':
+                continue
+        except:
+            print("&&")
         tr_testSentences.append(sentence)
 
 
